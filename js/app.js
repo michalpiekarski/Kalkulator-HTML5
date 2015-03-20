eb3ee912907191d24eed5f9714948c903fdaa067
Michał Piekarski <michal.piekarski@icloud.com>
2015-03-20 02:23:01 +0000

    Dodane wstawianie bloków do grafu. Dodatkowo refaktoryzacja podpięcia przełączania widoczności kategorii narzędzi

--- js/app.js
+++ js/app.js
@@ -7,3 +7,31 @@ function toggleToolsCategory(category) {
         event.currentTarget.style.cursor = "s-resize";
     }
 }
+
+function addToGraph(node) {
+    var newNode = document.createElement("div");
+    newNode.innerHTML = node.innerHTML;
+    newNode.setAttribute("class", "main-graph-node");
+    newNode.setAttribute("draggable", "true");
+    numberOfPlacedNodes++;
+    newNode.style.top = (25*numberOfPlacedNodes).toString()+"px";
+    newNode.style.left = (25*numberOfPlacedNodes).toString()+"px";
+    var graph = document.getElementById("main-graph");
+    graph.appendChild(newNode);
+}
+
+var numberOfPlacedNodes = 0;
+
+var nodes = document.getElementsByClassName("toolbox-tool");
+for(var i=0; i<nodes.length; i++) {
+    nodes[i].addEventListener("click", function() {
+        addToGraph(this);
+    });
+}
+
+var ttct = document.getElementsByClassName("toolbox-tools-category-title");
+for(var i=0; i<ttct.length; i++) {
+    ttct[i].addEventListener("click", function() {
+        toggleToolsCategory(this.nextElementSibling);
+    });
+}
