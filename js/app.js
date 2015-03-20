40911d22925bbee6430f36ebdb496016763e5f83
Michał Piekarski <michal.piekarski@icloud.com>
2015-03-20 01:53:29 +0000

    Dodane narzędzia w panelu narzędzi i ich stylizacja

---
+++ js/app.js
@@ -0,0 +1,9 @@
+function toggleToolsCategory(category) {
+    if(category.hasAttribute("hidden")) {
+        category.removeAttribute("hidden");
+        event.currentTarget.style.cursor = "n-resize";
+    } else {
+        category.setAttribute("hidden");
+        event.currentTarget.style.cursor = "s-resize";
+    }
+}
