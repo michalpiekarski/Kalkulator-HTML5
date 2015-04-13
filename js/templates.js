ed3df3ce10ba342d1bfca61a9fea9e1db2eb2e4d
Michał Piekarski <michal.piekarski@icloud.com>
2015-04-13 14:37:46 +0000

    Dodanie modelu szablonów zawartości bloków (js/templates.js) i zaimportowanie go w aplikacji (index.html)

---
+++ js/templates.js
@@ -0,0 +1,104 @@
+var  templates = {
+    "dodawanie" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='input-pin'></div>",
+                "<label>Y<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "odejmowanie" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='input-pin'></div>",
+                "<label>Y<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "mnozenie" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='input-pin'></div>",
+                "<label>Y<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "dzielenie" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='input-pin'></div>",
+                "<label>Y<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "negacja" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "potegowanie" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='input-pin'></div>",
+                "<label>Y<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "pierwiastkowanie" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='input-pin'></div>",
+                "<label>Y<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "logarytm" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='input-pin'></div>",
+                "<label>Y<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "silnia" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "modulo" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='input-pin'></div>",
+                "<label>Y<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "sinus" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "cosinus" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "tangens" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "cotangens" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "secans" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "cosecans" : {
+        "html": ["<div class='input-pin'></div>",
+                "<label>X<input type='number' /></label>",
+                "<div class='output-pin'></div>"]
+    },
+    "stala_pi" : {
+        "html": ["<div class='output-pin'></div>"]
+    },
+    "stala_epsilon" : {
+        "html": ["<div class='output-pin'></div>"]
+    }
+};
