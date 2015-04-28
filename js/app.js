6ac7fe6cc0905a09c3ee189a84752696b225060c
Michał Piekarski <michal.piekarski@icloud.com>
2015-04-28 22:10:11 +0000

    Rozwiązany problem odwracania połączenia w pionie i poziomie. Formatowanie kodu

--- js/app.js
+++ js/app.js
@@ -1,8 +1,9 @@
     function toggleToolsCategory(category, event) {
-        if(category.hasAttribute("hidden")) {
+        if (category.hasAttribute("hidden")) {
             category.removeAttribute("hidden");
             event.currentTarget.style.cursor = "n-resize";
-        } else {
+        }
+        else {
             category.setAttribute("hidden", "hidden");
             event.currentTarget.style.cursor = "s-resize";
         }
@@ -12,136 +13,115 @@
         var xPosition = 0;
         var yPosition = 0;
 
-        while(element) {
+        while (element) {
             xPosition += (element.offsetLeft - element.scrollLeft + element.clientLeft);
             yPosition += (element.offsetTop - element.scrollTop + element.clientTop);
             element = element.offsetParent;
         }
-        return { x: xPosition, y: yPosition };
+        
+        return {
+            x: xPosition,
+            y: yPosition
+        };
     }
 
     var Connecting = false;
     var ConnectionSettings = {
-        "Start": [0,0],
-        "End": [0,0],
         "StartPin": null,
         "EndPin": null
     };
-
     var ConnectionNum = 0;
 
-
-    function CreateConnection(connection){
-
-
+    function CreateConnection(connection) {
         var start_pos = getPosition(document.getElementById(connection.dataset.StartPin));
         var end_pos = getPosition(document.getElementById(connection.dataset.EndPin));
         var width = Math.abs(end_pos.x - start_pos.x);
-            var height = Math.abs(end_pos.y - start_pos.y);
-            var top = Math.min(start_pos.y, end_pos.y);
-            var left = Math.min(start_pos.x, end_pos.x);
-
-         connection.setAttribute("height", height);      
-            connection.setAttribute("width", width);
-            connection.style.width = width.toString() + "px";
-            connection.style.height = height.toString() + "px";
-            
-            connection.style.position = "absolute";
-            connection.style.top = top.toString() + "px";
-            connection.style.left = left.toString() + "px";
-
-         
-         var ctx = connection.getContext("2d");
-         ctx.clearRect(0,0,width, height);
-         ctx.moveTo(0,0);
-         ctx.strokeStyle = "white";
-         ctx.bezierCurveTo(width/2, 0, width/2, height, width, height);
-         ctx.stroke();
-
+        var height = Math.abs(end_pos.y - start_pos.y);
+        var top = Math.min(start_pos.y, end_pos.y);
+        var left = Math.min(start_pos.x, end_pos.x);
+        var inverse_v = top < start_pos.y ? 1 : 0;
+        var inverse_h = left < start_pos.x ? 1 : 0;
+        
+        connection.setAttribute("height", height);
+        connection.setAttribute("width", width);
+        connection.style.width = width.toString() + "px";
+        connection.style.height = height.toString() + "px";
+        connection.style.position = "absolute";
+        connection.style.top = top.toString() + "px";
+        connection.style.left = left.toString() + "px";
+        
+        var ctx = connection.getContext("2d");
+        ctx.clearRect(0, 0, width, height);
+        ctx.moveTo(
+            width * inverse_h,
+            height * inverse_v
+            );
+        ctx.strokeStyle = "white";
+        ctx.bezierCurveTo(
+            width / 2,
+            height * inverse_v,
+            width / 2,
+            height * (1 - inverse_v),
+            width * (1 - inverse_h),
+            height * (1 - inverse_v)
+            );
+        ctx.stroke();
     }
 
     function ConnectionClick(event) {
-        if(event.target.getAttribute("class").indexOf("pin") > -1)
-        {
-
-
-           if(!event.currentTarget.hasAttribute("id") && Connecting) 
-            {
-            Connecting = false; 
-            ConnectionSettings.End = [event.clientX, event.clientY];
-
-            var Graph = document.getElementById("main-graph");
-            
-            var ConnectionNode = document.createElement("canvas");
-            ConnectionNode.setAttribute("class", "main-graph-connection");
-            
-            ConnectionNode.id = "connection" + ConnectionNum.toString();
-
-
-            
-
-           
-
-            event.currentTarget.id = "PinIn" + ConnectionNum.toString();
-            ConnectionNum++;
-            
-            ConnectionSettings.EndPin = event.currentTarget;
-            console.log(ConnectionSettings.StartPin.id + ConnectionSettings.EndPin.id);
-            ConnectionNode.dataset.StartPin = ConnectionSettings.StartPin.id;
-            ConnectionNode.dataset.EndPin = ConnectionSettings.EndPin.id;
-
-
-
-
-            CreateConnection(ConnectionNode);
-            Graph.appendChild(ConnectionNode);
-
-            
-
-
+        if (event.target.getAttribute("class").indexOf("pin") > -1 && !event.currentTarget.hasAttribute("id")) {
+            if (Connecting) {
+                console.log("end connection");
+                Connecting = false;
+                
+                var ConnectionNode = document.createElement("canvas");
+                ConnectionNode.setAttribute("class", "main-graph-connection");
+                ConnectionNode.id = "connection" + ConnectionNum.toString();
+                event.currentTarget.id = "PinIn" + ConnectionNum.toString();
+                ConnectionNum++;
+                
+                ConnectionSettings.EndPin = event.currentTarget;
+                ConnectionNode.dataset.StartPin = ConnectionSettings.StartPin.id;
+                ConnectionNode.dataset.EndPin = ConnectionSettings.EndPin.id;
+                
+                CreateConnection(ConnectionNode);
+                
+                var Graph = document.getElementById("main-graph");
+                Graph.appendChild(ConnectionNode);
             }
-
-
-            else if (!event.currentTarget.hasAttribute("id"))
-            {
-            Connecting = true;
-            ConnectionSettings.Start = [event.clientX, event.clientY];
-            event.currentTarget.id = "PinOut" + ConnectionNum.toString();
-            ConnectionSettings.StartPin = event.currentTarget;
-            
+            else {
+                console.log("start connection");
+                Connecting = true;
+                
+                event.currentTarget.id = "PinOut" + ConnectionNum.toString();
+                ConnectionSettings.StartPin = event.currentTarget;
             }
-
-            else
-            {
-
-            }
-        
         }
-
         else {
+            console.log("connection error");
             event.PreventDefault();
+        }
     }
-}
 
-function nodeDragStart(event) {
-    if(event.target.getAttribute("class") === "drag-handle") {
-        var clone = event.currentTarget.cloneNode(true);
-        clone.style.visibility = "hidden";
-        clone.style.overflow = "hidden";
-        clone.id ="clone-node";
-        event.currentTarget.appendChild(clone);
-        event.dataTransfer.setDragImage(clone,0,0);
-        event.dataTransfer.setData("text/plain","głupi firefox");
-    } else {
+    function nodeDragStart(event) {
+        if (event.target.getAttribute("class") === "drag-handle") {
+            var clone = event.currentTarget.cloneNode(true);
+            clone.style.visibility = "hidden";
+            clone.style.overflow = "hidden";
+            clone.id = "clone-node";
+            event.currentTarget.appendChild(clone);
+            event.dataTransfer.setDragImage(clone, 0, 0);
+            event.dataTransfer.setData("text/plain", "głupi firefox");
+        }
+        else {
+            event.preventDefault();
+        }
+    }
+
+    function nodeDrag(event) {
         event.preventDefault();
-    }
-}
-
-function nodeDrag(event) {
-    event.preventDefault();
-    this.style.top = (cursorPos.y-this.offsetHeight/2).toString() + "px";
-    this.style.left = (cursorPos.x-this.offsetWidth/2).toString() + "px";
-        // this.innerHTML = this.id + "<br/>" + cursorPos.x + ", " + cursorPos.y;
+        this.style.top = (cursorPos.y - this.offsetHeight / 2).toString() + "px";
+        this.style.left = (cursorPos.x - this.offsetWidth / 2).toString() + "px";
     }
 
     function nodeDragEnd(event) {
@@ -152,28 +132,25 @@ function nodeDrag(event) {
         for (var i = 0; i < pins.length; i++) {
             if (pins[i].hasAttribute("id")) {
                 if (pins[i].id.indexOf("In") > -1) {
-
                     var connection_num = "connection" + pins[i].id.substr(5);
                     CreateConnection(document.getElementById(connection_num));
                 }
-                else if (pins[i].id.indexOf("Out") > -1){
-
+                else if (pins[i].id.indexOf("Out") > -1) {
                     var connection_num = "connection" + pins[i].id.substr(6);
                     CreateConnection(document.getElementById(connection_num));
-
                 }
             }
-        };
+        }
     }
 
     function addToGraph(node) {
         var newNode = document.createElement("div");
         newNode.setAttribute("class", "main-graph-node");
-        newNode.id = "node"+numberOfPlacedNodes;
+        newNode.id = "node" + numberOfPlacedNodes;
         numberOfPlacedNodes++;
-        newNode.innerHTML = node.innerHTML + "<br/>" + newNode.id +templates[node.dataset.template].join("\n");
-        newNode.style.top = (25*numberOfPlacedNodes).toString()+"px";
-        newNode.style.left = (25*numberOfPlacedNodes).toString()+"px";
+        newNode.innerHTML = node.innerHTML + "<br/>" + newNode.id + templates[node.dataset.template].join("\n");
+        newNode.style.top = (25 * numberOfPlacedNodes).toString() + "px";
+        newNode.style.left = (25 * numberOfPlacedNodes).toString() + "px";
         newNode.setAttribute("draggable", "true");
         newNode.addEventListener("dragstart", nodeDragStart, false);
         newNode.addEventListener('drag', nodeDrag);
@@ -186,8 +163,8 @@ function nodeDrag(event) {
         var graph = document.getElementById("main-graph");
         var nodes = graph.getElementsByClassName("main-graph-node");
         var nodesNum = nodes.length;
-        if(nodesNum > 0) {
-            for(var i=0; i<nodesNum; i++) {
+        if (nodesNum > 0) {
+            for (var i = 0; i < nodesNum; i++) {
                 nodes[0].removeEventListener("dragstart", nodeDragStart);
                 nodes[0].removeEventListener("drag", nodeDrag);
                 nodes[0].removeEventListener("dragend", nodeDragEnd);
@@ -197,8 +174,7 @@ function nodeDrag(event) {
         numberOfPlacedNodes = 0;
     }
 
-    function deleteNode(node){
-
+    function deleteNode(node) {
         var graph = document.getElementById("main-graph");
         var allnodes = graph.getElementsByClassName("main-graph-node");
         var nodesNum = allnodes.length;
@@ -208,13 +184,10 @@ function nodeDrag(event) {
         var index = parseInt(node.id.substr(4));
         graph.removeChild(node);
         numberOfPlacedNodes--;
-        if(numberOfPlacedNodes>0&&index<nodesNum-1)
-        {
-            for(var i=index;i<nodesNum-1;i++)
-                allnodes[i].id="node"+i;
-
+        if (numberOfPlacedNodes > 0 && index < nodesNum - 1) {
+            for (var i = index; i < nodesNum - 1; i++)
+                allnodes[i].id = "node" + i;
         }
-        
     }
 
 
@@ -225,7 +198,7 @@ function nodeDrag(event) {
     };
 
     var graph = document.getElementById("main-graph");
-    graph.addEventListener('dragover', function(event){
+    graph.addEventListener('dragover', function(event) {
         event.preventDefault();
         cursorPos.x = event.clientX;
         cursorPos.y = event.clientY;
@@ -235,23 +208,22 @@ function nodeDrag(event) {
     });
 
     var nodes = document.getElementsByClassName("toolbox-tool");
-    for(var i=0; i<nodes.length; i++) {
+    for (var i = 0; i < nodes.length; i++) {
         nodes[i].addEventListener("click", function() {
             addToGraph(this);
         });
-        nodes[i].addEventListener("mousemove",ToolDescription);
+        nodes[i].addEventListener("mousemove", ToolDescription);
     }
 
     var ttct = document.getElementsByClassName("toolbox-tools-category-title");
-    for(var i=0; i<ttct.length; i++) {
+    for (var i = 0; i < ttct.length; i++) {
         ttct[i].addEventListener("click", function(event) {
             toggleToolsCategory(this.nextElementSibling, event);
         });
     }
 
-    function ToolDescription(event)
-    {
+    function ToolDescription(event) {
         var opis = event.currentTarget.nextElementSibling;
-        opis.style.top=event.clientY+"px";
-        opis.style.left=(event.clientX - opis.offsetWidth-20)+"px";
+        opis.style.top = event.clientY + "px";
+        opis.style.left = (event.clientX - opis.offsetWidth - 20) + "px";
     }
